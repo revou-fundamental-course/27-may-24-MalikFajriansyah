685c1049b82b650a081a633243833cfbf734227b
MalikFajriansyah <malik04fajriansyah@gmail.com>
2024-06-05 20:22:13 +0700

    add function for validasi nama

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 document.getElementById('contactForm').addEventListener('submit', function (events) {
     events.preventDefault();
 
+    //validasi function untuk enter nama
+    showNameInput();
+
+
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var message = document.getElementById('message').value;
@@ -28,9 +32,29 @@ document.getElementById('contactForm').addEventListener('submit', function (even
 
     // Reset formulir setelah pengisian
     document.getElementById('contactForm').reset();
-
     // Hapus notifikasi setelah 3 detik
     setTimeout(function() {
         notification.remove();
     }, 3000);
 })
+
+// Fungsi untuk menampilkan form input nama saat halaman dimuat
+function showNameInput() {
+    const name = localStorage.getItem('userName');
+
+    if (!name) {
+        const userName = prompt("Please enter your name:");
+        if (userName) {
+            localStorage.setItem('userName', userName);
+            updateWelcomeMessage(userName);
+        }
+    } else {
+        updateWelcomeMessage(name);
+    }
+}
+
+// Fungsi untuk memperbarui pesan selamat datang dengan nama pengguna
+function updateWelcomeMessage(name) {
+    const welcomeMessage = document.getElementById('welcomeMessage');
+    welcomeMessage.textContent = `Welcome ${name}`;
+}
